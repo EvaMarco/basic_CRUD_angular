19e94a2afddd9b5cf55950f4040f1671ca556f61
Migara <evamarcod@gmail.com>
2019-11-02 13:56:15 +0100

    Add finishing touches

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserList } from './user-list/userList.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
+import { UserDeleteComponent } from './user-delete/user-delete.component';
 import { UserAddComponent } from './user-add/user-add.component';
 import { UserEditComponent } from './user-edit/user-edit.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -17,6 +18,7 @@ import { MessagesComponent } from './messages/messages.component';
     UserAddComponent,
     UserEditComponent,
     MessagesComponent,
+    UserDeleteComponent,
     UserDetailComponent
   ],
   imports: [
